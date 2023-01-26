3a9e8c3c3bf6d0d620c12c899963a69733c2c380
Mareike Heuer <mareike.heuer@hotmail.com>
2023-01-26 13:55:42 +0100

    Add styling to Course Card component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -11,19 +11,25 @@ function CourseCard() {
       .then((res) => res.json())
       .then((apiData) => {
         setCourseCardData(apiData);
-        console.log(apiData);
       });
   }, []);
 
   return (
-    <div>
+    <div className="container">
       {courseCardData &&
         courseCardData.map((apiData) => (
-          <div key={uuidv4()}>
-            <h2>{apiData.title}</h2>
-            <p>{apiData.next_start_formatted}</p>
+          <div key={uuidv4()} className="course-card">
+            <div className="course-card--top">
+              <h2>{apiData.title}</h2>
+            </div>
+            <div className="course-card--middle">
+              <h3>Start Date:</h3>
+              <p>{apiData.next_start_formatted}</p>
+            </div>
             <Link to={`/courses/${apiData.slug}`}>
-              <button>View program &#8594;</button>
+              <button className="course-card--button">
+                View program &#8594;
+              </button>
             </Link>
           </div>
         ))}
