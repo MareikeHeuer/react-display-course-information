de96b0c4a6c9d5ab673f8720a0206f648042cd22
Mareike Heuer <mareike.heuer@hotmail.com>
2023-01-26 10:12:42 +0100

    Make it work first. Display course title & description from hard-coded data array

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 
 function CourseCard() {
+  const courseCardData = [
+    {
+      slug: "voice-user-interface-design-with-amazon-alexa",
+      title: "Voice User Interface Design",
+      url: "https://careerfoundry.com/en/courses/voice-user-interface-design-with-amazon-alexa/",
+      next_start: "2022-10-10",
+      next_start_formatted: "Monday, October 10th, 2022",
+    },
+    {
+      slug: "full-stack-immersion",
+      title: "Full-Stack Immersion",
+      url: "https://careerfoundry.com/en/courses/become-a-web-developer/",
+      next_start: "2022-10-17",
+      next_start_formatted: "Monday, October 17th, 2022",
+    },
+  ];
+
   return (
     <div>
-      <h1>This is the Course Card component</h1>
+      {courseCardData.map((data) => (
+        <div>
+          <h2>{data.title}</h2>
+          <p>{data.next_start_formatted}</p>
+        </div>
+      ))}
     </div>
   );
 }
