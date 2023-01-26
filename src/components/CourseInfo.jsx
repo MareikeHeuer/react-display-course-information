8ee09e01db6662cee1f1e628601f2e4fbbabd5c0
Mareike Heuer <mareike.heuer@hotmail.com>
2023-01-26 11:56:02 +0100

    Make it work. Fetch request to CF course info API, display data specific to course based on slug in console

--- src/components/CourseInfo.jsx
+++ src/components/CourseInfo.jsx
@@ -1,6 +1,21 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function CourseInfo() {
+  const [courseInfoData, setCourseInfoData] = useState(null);
+  const { slug } = useParams();
+  console.log(slug);
+
+  useEffect(() => {
+    fetch(`https://private-e05942-courses22.apiary-mock.com/courses/${slug}`)
+      .then((res) => res.json())
+      .then((apiData) => {
+        setCourseInfoData(apiData);
+        console.log(apiData);
+      });
+  }, [slug]);
+
   return (
     <div>
       <h1>This is the course info component</h1>
