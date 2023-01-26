b6021b75ebd58a710efe1b02ef383888508c539d
Mareike Heuer <mareike.heuer@hotmail.com>
2023-01-26 10:27:04 +0100

    Replace hard-coded data array with fetch request to CF API

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,31 +1,27 @@
 import React from "react";
+import { useState, useEffect } from "react";
 
 function CourseCard() {
-  const courseCardData = [
-    {
-      slug: "voice-user-interface-design-with-amazon-alexa",
-      title: "Voice User Interface Design",
-      url: "https://careerfoundry.com/en/courses/voice-user-interface-design-with-amazon-alexa/",
-      next_start: "2022-10-10",
-      next_start_formatted: "Monday, October 10th, 2022",
-    },
-    {
-      slug: "full-stack-immersion",
-      title: "Full-Stack Immersion",
-      url: "https://careerfoundry.com/en/courses/become-a-web-developer/",
-      next_start: "2022-10-17",
-      next_start_formatted: "Monday, October 17th, 2022",
-    },
-  ];
+  const [courseCardData, setCourseCardData] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://private-e05942-courses22.apiary-mock.com/courses`)
+      .then((res) => res.json())
+      .then((apiData) => {
+        setCourseCardData(apiData);
+        console.log(apiData);
+      });
+  }, []);
 
   return (
     <div>
-      {courseCardData.map((data) => (
-        <div>
-          <h2>{data.title}</h2>
-          <p>{data.next_start_formatted}</p>
-        </div>
-      ))}
+      {courseCardData &&
+        courseCardData.map((apiData) => (
+          <div>
+            <h2>{apiData.title}</h2>
+            <p>{apiData.next_start_formatted}</p>
+          </div>
+        ))}
     </div>
   );
 }
