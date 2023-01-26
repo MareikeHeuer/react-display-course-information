206e8db087246df4bc13f0b85e05179be416b14c
Mareike Heuer <mareike.heuer@hotmail.com>
2023-01-26 10:59:20 +0100

    Create CourseInfo component and render in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import CourseCard from "./components/CourseCard";
+import CourseInfo from "./components/CourseInfo";
 
 function App() {
   return (
     <div>
       <CourseCard />
+      <CourseInfo />
     </div>
   );
 }
